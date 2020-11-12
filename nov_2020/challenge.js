5d5b6afe37b76fdd2e1cbc34f696c2133aa31e12
robdcampbell <rdeancampbell@gmail.com>
2020-11-11 23:11:23 -0500

    Closures and Prototypes continued. Onto more OOP and Functional programming next

---
+++ nov_2020/challenge.js
@@ -0,0 +1,33 @@
+
+
+function generation(x, y) {
+
+  const m = {
+    "-3": "great grandfather",
+    "-2": "grandfather",
+    "-1": "father",
+    "0": "me!",
+    "1": "son",
+    "2": "grandson",
+    "3": "great grandson" 
+  }
+
+  const f = {
+    "-3": "great grandmother",
+    "-2": "grandmother",
+    "-1": "mother",
+    "0": "me!",
+    "1": "daughter",
+    "2": "granddaughter", 
+    "3": "great granddaughter" 
+  }
+
+  let gender;
+  y === 'f' ? gender = f : gender = m; 
+
+  
+  return gender[x.toString()];
+
+}
+
+console.log(generation(-3,'f'))
--- nov_2020/oop.js
+++ nov_2020/oop.js
@@ -1,29 +1,129 @@
 
-// HIGHER ORDER FUNCTIONS
+// PILLARS OF JS: exploring Closures and prototypes 
 
-//Written the original way
-  /*
-  function subtract(num1){
-      return function(num2){
-        return num1-num2;
-      }
-    }
-  */
- 
-// Using arrow functions 
-    /*
-      const subtract = (num1) => {
-        return (num2) =>{
-          return num1*num2;
+
+
+
+    // but first...
+        // HIGHER ORDER FUNCTIONS - a function that returns another function 
+            /*
+              Although they seem 'Trickier' at first, they provide a greater flexibility syntax (and are thus powerful).
+            */
+
+          //Written the original way
+            /*
+            function subtract(num1){
+                return function(num2){
+                  return num1-num2;
+                }
+              }
+            */
+          
+          // Using arrow functions 
+              /*
+                const subtract = (num1) => {
+                  return (num2) =>{
+                    return num1*num2;
+                  }
+                }
+              */
+
+              // One line arrow function:
+            /*
+            const subtract = (num1)=> (num2)=> num1-num2;
+
+            const num1 = subtract(6)
+            console.log(num1(10));
+            */
+
+// CLOSURES : advantages: don't pollute the global scope, create private variables
+
+/* 
+      Memory Efficient 
+
+
+      Encapsulation 
+
+*/
+
+//Encapsulation 
+  // example 1
+      /*
+      let view;
+      function initialize(){
+        if(view !== -1){
+          console.log('view has be set!!!')
+          view = -1;
         }
       }
-    */
 
-    // One line arrow function:
-  const subtract = (num1)=> (num2)=> num1-num2;
+      initialize();
+      initialize();
+      initialize();
+      console.log(view);
+      */
+  
+  // Example 2
+
+  // const array = [1,2,3,4];
+  // for (let i = 0; i <array.length; i++){
+  //   setTimeout(()=> {
+  //     console.log(`I am at index ${i}`)
+  //   },1000)
+  // }
 
 
-const num1 = subtract(6)
-console.log(num1(10));
 
+  // OOP.
+      /*
+        const dragon = {
+          name: 'Tanya',
+          fire: true,
+          fight(){
+            return 5;
+          },
+          sing(){
+            if(this.fire){
+              return `I am ${this.name}, breather of fire.`;
+            } else {
+              return `No fire for you ${this.name}!`;
+            }
+          }
+        }
 
+        const lizard = {
+          name: 'Kiki',
+          fight(){
+            return 1;
+          }
+        }
+      */
+
+  // Using Bind, to inherit the sing method (from Dragon) to lizard.
+    // const lizardSing = dragon.sing.bind(lizard);
+    // console.log(lizardSing());
+  
+  // Using prototypal inheritance:
+    // Everything will be inherited unless specifically overwritten in the inheriting object
+    // !!!! Warning, this next code is just an example, do not use in production! 
+     
+      // lizard.__proto__ = dragon;
+   /*
+    for (let prop in lizard) {
+      // Will only show the properties this object has defined, not inherited.
+      if(lizard.hasOwnProperty(prop)){
+        console.log(prop);
+      }
+    }
+
+    console.log(dragon.isPrototypeOf(lizard));
+  
+  */
+
+  Date.prototype.lastYear = function(){
+    return this.getFullYear() - 1;
+  }
+  console.log(Date.prototype);
+
+  console.log(new Date('1900-10-10').getFullYear());
+  console.log(new Date('1900-10-10').lastYear());
